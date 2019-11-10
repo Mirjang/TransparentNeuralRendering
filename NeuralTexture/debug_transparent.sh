7a53e95be90f52fb10079c59f6c00f3e60b7b832
Patrick Radner <radner.patrick@gmail.com>
2019-11-10 18:15:52 +0100

    updated linux

--- NeuralTexture/debug_transparent.sh
+++ NeuralTexture/debug_transparent.sh
@@ -3,7 +3,7 @@ set -ex
 DATASETS_DIR=/home/mirjang/Desktop/NeuralTextures/TransparentNeuralRendering/Data
 # objects
 DATA=debug
-
+NUM_OBJECTS=3 
 # renderer
 RENDERER=MultiTarget-neuralRenderer_200
 
@@ -17,4 +17,4 @@ LR=0.001
 # GPU
 GPU_ID=0
 
-python debug.py --fix_renderer --niter 2000 --dataroot $DATASETS_DIR/$OBJECT --name $OBJECT-$MODEL --renderer $RENDERER --model $MODEL --netG unet_256 --lambda_L1 100 --dataset_mode transparent --no_lsgan --norm batch --pool_size 0 --gpu_ids $GPU_ID --lr $LR
+python debug.py --nObjects $NUM_OBJECTS --fix_renderer --niter 2000 --dataroot $DATASETS_DIR/$DATA --name $OBJECT-$MODEL --renderer $RENDERER --model $MODEL --netG unet_256 --lambda_L1 100 --dataset_mode transparent --no_lsgan --norm batch --pool_size 0 --gpu_ids $GPU_ID --lr $LR
