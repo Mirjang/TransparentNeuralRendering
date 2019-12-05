ee9eb22c4afd9365fecbaae18caad8e296603021
Patrick Radner <radner.Patrick@gmaiL.com>
2019-12-05 15:41:37 +0100

    switched from debug to train.py for server

--- NeuralTexture/debug_transparent.sh
+++ NeuralTexture/debug_transparent.sh
@@ -9,18 +9,18 @@ DATASETS_DIR=/mnt/raid/patrickradner/datasets
 LR=0.001
 BATCH_SIZE=1
 # renderer
-RENDERER=MultiTarget-neuralRenderer_200
+#RENDERER=MultiTarget-neuralRenderer_200
+RENDERER=no_renderer
 # GPU
 GPU_ID="0"
 
 # display params
 DISP_FREQ=100
 
-bash "./experiment_setups/LstmPerPixel4_lab_2.bat"
-# bash "./experiment_setups/PerPixel4_lab_2.bat"
-# bash "./experiment_setups/UNET_5_lab_2.bat"
-# bash "./experiment_setups/Blend_lab_2.bat"
-# bash "./experiment_setups/Debug.bat"
+bash "./experiment_setups/LstmPerPixel4_lab_2.sh"
+ #bash "./experiment_setups/PerPixel4_lab_2.sh"
+ #bash "./experiment_setups/UNET_5_lab_2.sh"
+ #bash "./experiment_setups/Blend_lab_2.sh"
+ #bash "./experiment_setups/Debug.sh"
 
-
-python debug.py --niter 500 --save_epoch_freq 10 --rendererType $RENDERER_TYPE --batch_size $BATCH_SIZE --nObjects $NUM_OBJECTS --tex_dim $TEX_DIM --tex_features $TEX_FEATURES --dataroot $DATASETS_DIR/$DATA --name $NAME --num_depth_layers $NUM_DEPTH_LAYERS --renderer $RENDERER --model $MODEL --netG unet_256 --lambda_L1 100 --dataset_mode $DATASET_MODE --no_lsgan --norm batch --pool_size 0 --gpu_ids $GPU_ID --lr $LR --display_freq $DISP_FREQ --print_freq $DISP_FREQ $CONTINUE $OPTIONS
+python train.py --niter 500 --save_epoch_freq 10 --rendererType $RENDERER_TYPE --batch_size $BATCH_SIZE --nObjects $NUM_OBJECTS --tex_dim $TEX_DIM --tex_features $TEX_FEATURES --dataroot $DATASETS_DIR/$DATA --name $NAME --num_depth_layers $NUM_DEPTH_LAYERS --renderer $RENDERER --model $MODEL --netG unet_256 --lambda_L1 100 --dataset_mode $DATASET_MODE --no_lsgan --norm batch --pool_size 0 --gpu_ids $GPU_ID --lr $LR --display_freq $DISP_FREQ --print_freq $DISP_FREQ $CONTINUE $OPTIONS
--- NeuralTexture/test_transparent.sh
+++ NeuralTexture/test_transparent.sh
@@ -4,11 +4,12 @@ DATASETS_DIR=/mnt/raid/patrickradner/datasets
 
 
  #renderer
-RENDERER=MultiTarget-neuralRenderer_200
+#RENDERER=MultiTarget-neuralRenderer_200
+RENDERER=no_renderer
 
  #optimizer parameters
 LR=0.001
-BATCH_SIZE=1
+shCH_SIZE=1
 
  #GPU
 GPU_ID="0"
@@ -19,11 +20,11 @@ LOSS=L1
 
 EPOCH=latest
 
-bash "./experiment_setups/LstmPerPixel4_lab_2.bat"
- #bash "./experiment_setups/PerPixel4_lab_2.bat"
- #bash "./experiment_setups/UNET_5_lab_2.bat"
- #bash "./experiment_setups/Blend_lab_2.bat"
- #bash "./experiment_setups/Debug.bat"
+bash "./experiment_setups/LstmPerPixel4_lab_2.sh"
+ #bash "./experiment_setups/PerPixel4_lab_2.sh"
+ #bash "./experiment_setups/UNET_5_lab_2.sh"
+ #bash "./experiment_setups/Blend_lab_2.sh"
+ #bash "./experiment_setups/Debug.sh"
 
 
 python test.py --nObjects $NUM_OBJECTS --use_extrinsics --rendererType $RENDERER_TYPE --num_depth_layers $NUM_DEPTH_LAYERS --name $NAME --epoch $EPOCH --display_winsize 512 --nObjects $NUM_OBJECTS --tex_dim $TEX_DIM --tex_features $TEX_FEATURES --dataroot $DATASETS_DIR/$DATA  --lossType $LOSS --model $MODEL --netG unet_256 --dataset_mode $DATASET_MODE --norm batch  --gpu_ids $GPU_ID $OPTIONS
