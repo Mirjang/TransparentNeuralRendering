5577c4e95376bb156ec9103908219080039f11d1
Patrick Radner <radner.patrick@gmail.com>
2020-01-13 13:24:58 +0100

    interpolating dataset renderer

--- Rendering/Assets/Scripts/CameraScripts/SampleImages.cs
+++ Rendering/Assets/Scripts/CameraScripts/SampleImages.cs
@@ -14,8 +14,10 @@ public class SampleImages : MonoBehaviour
     public float minAngleX = -90;
     public float maxAngleX = 90;
 
-    public Vector3 centerRadius = Vector3.one; 
-
+    public Vector3 centerRadius = Vector3.one;
+    public bool smoothPath = false;
+    public float smoothSpeed = 50f;
+    public float smoothYSteps = 15.0f; 
     public bool saveImages = true;
     public enum ActionOnFinish { None, Exit, LoadScene };
 
@@ -31,11 +33,19 @@ public class SampleImages : MonoBehaviour
 
     private int frameCounter = 0; 
 
+    private float smoothAngX, smoothAngY;
+    private float smoothDirX = 1.0f, smoothDirY = 1.0f;
+    private float smoothYStep = 0.0f; 
+    private float smoothPathStartTime = 0.0f;
+    private float goDirX = 1.0f; 
     // Start is called before the first frame update
     void Start()
     {
         cam = Instantiate(camPrefab);
         render = cam.GetComponentInChildren<CustomRender>();
+        smoothAngX = minAngleX;
+        smoothAngY = minAngleY;
+        smoothPathStartTime = Time.time; 
     }
 
     // Update is called once per frame
@@ -81,10 +91,51 @@ public class SampleImages : MonoBehaviour
             return;
         }
 
+        float r, angX, angY;
+        if (smoothPath)
+        {
+            r = (minRadius + maxRadius) / 2.0f;
+            angX = smoothAngX;
+            angY = smoothAngY;
+            smoothAngY += smoothSpeed * smoothDirX * goDirX;
+            if ((smoothAngY > maxAngleY || smoothAngY < minAngleY) && goDirX > 0)
+            {
+                smoothAngY = Mathf.Clamp(smoothAngY, minAngleY, maxAngleY);
+                smoothDirX *= -1.0f;
+                goDirX = 0.0f;
+            }
 
-        float r = Random.Range(minRadius, maxRadius);
-        float angX = Random.Range(minAngleX, maxAngleX);
-        float angY = Random.Range(minAngleY, maxAngleY);
+            if (goDirX < 1)
+            {                         
+                float yStep = smoothSpeed * smoothDirY;
+                smoothYStep += yStep; 
+                if(smoothYStep > smoothYSteps)
+                {
+                    yStep = smoothYStep - smoothYSteps;
+                    smoothYStep = 0;
+                    goDirX = 1.0f; 
+                }
+                smoothAngX += yStep; 
+
+            }
+
+            if(smoothAngX > maxAngleX || smoothAngY > maxAngleY)
+            {
+                RenderOptions.getInstance().numFrames = frameCounter;
+                return; 
+            }
+
+        }
+        else
+        {
+            r = Random.Range(minRadius, maxRadius);
+            angX = Random.Range(minAngleX, maxAngleX);
+            angY = Random.Range(minAngleY, maxAngleY);
+        }
+
+
+
+
 
         cam.transform.rotation = transform.rotation;
         cam.transform.Rotate(angX, angY, 0f);
