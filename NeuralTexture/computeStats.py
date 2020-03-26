b95d3adcc4bb89f464a86958ebcea98fdf0e4a78
Mirjang <radner.patrick@gmail.com>
2020-03-26 21:33:42 +0100

    imporved psnr script

--- NeuralTexture/computeStats.py
+++ NeuralTexture/computeStats.py
@@ -17,68 +17,65 @@ def main():
     if(len(sys.argv) < 2): 
         print("call this thing with file containing paths to frames")
         exit()
+    if os.path.isdir(sys.argv[1]): 
+        dirs = [sys.argv[1]]
+    elif os.path.isfile(sys.argv[1]): 
+        with open(sys.argv[1]) as f: 
+            dirs = f.readlines()
+    else: 
+        print("woot")
+    print("comparing: " + str(dirs))
 
-    with open(sys.argv[1]) as f: 
-        dirs = f.readlines()
-        print("comparing: " + str(dirs))
+    loss_hist = None
+    i = 0
+    done = False
+    while not done: 
+        if(i%50==0): 
+            print("Frame: " + str(i))
+        gt_count = 0
+        display = 0
+        pdiri = ""
+        gt = None
+        frames = []
+        for diri in dirs: 
+            diri = diri.strip()
 
-        nhf = len(dirs)+1
-        if(len(sys.argv) > 2): 
-            nhf = int(sys.argv[2])
-        out_file = str(sys.argv[1]) + ".avi"
-        if(os.path.exists(out_file)): 
-            os.remove(out_file)
-
-        loss_hist = None
-        i = 0
-        done = False
-        while not done: 
-            if(i%5==0): 
-                print("Frame: " + str(i))
-            gt_count = 0
-            display = 0
-            pdiri = ""
-            gt = None
-            frames = []
-            for diri in dirs: 
-                diri = diri.strip()
-
-                if diri[:3] == "GT:": 
-                    diri = diri[3:]
-                    framediri = os.path.join(diri, str(i) + "_rgb_target.png")
-                    gt_count += 1
-                    if (not os.path.exists(framediri)):
-                        print("Ended at: " + str(framediri))
-                        done = True
-                    else: 
-                        gt = cv2.imread(framediri)
-                elif diri == "GT": 
-                    framediri = os.path.join(pdiri, str(i) + "_rgb_target.png")
-                    gt_count += 1
-                    if (not os.path.exists(framediri)):
-                        print("Ended at: " + str(framediri))
-                        done = True
-                    else: 
-                        gt = cv2.imread(framediri)
-
+            if diri[:3] == "GT:": 
+                diri = diri[3:]
+                framediri = os.path.join(diri, str(i) + "_rgb_target.png")
+                gt_count += 1
+                if (not os.path.exists(framediri)):
+                    print("Ended at: " + str(framediri))
+                    done = True
                 else: 
-                    framediri = os.path.join(diri, str(i) + "_rgb_fake.png")
-                    pdiri = diri
-                    if (not os.path.exists(framediri)):
-                        print("Ended at: " + str(framediri))
-                        done = True
-                    else: 
-                        frames.append(cv2.imread(framediri))
-            
-            if gt_count <1 : 
-                gt = cv2.imread(os.path.join(diri, str(i) + "_rgb_target.png"))
+                    gt = cv2.imread(framediri)
+            elif diri == "GT": 
+                framediri = os.path.join(pdiri, str(i) + "_rgb_target.png")
+                gt_count += 1
+                if (not os.path.exists(framediri)):
+                    print("Ended at: " + str(framediri))
+                    done = True
+                else: 
+                    gt = cv2.imread(framediri)
 
-            if not loss_hist: # 1st iter
-                loss_hist = [[]*len(frames)]
-            for i, frame in enumerate(frames): 
-                err = psnr(frame, gt)
-                loss_hist[i].append(err) 
-            i+=1
+            else: 
+                framediri = os.path.join(diri, str(i) + "_rgb_fake.png")
+                pdiri = diri
+                if (not os.path.exists(framediri)):
+                    print("Ended at: " + str(framediri))
+                    done = True
+                else: 
+                    frames.append(cv2.imread(framediri))
+        
+        if gt_count <1 : 
+            gt = cv2.imread(os.path.join(diri, str(i) + "_rgb_target.png"))
+        if not loss_hist: # 1st iter
+            loss_hist = [[] for _ in range(len(frames))]
+
+        for x, frame in enumerate(frames): 
+            err = psnr(frame, gt)
+            loss_hist[x].append(err) 
+        i+=1
 
     # done computing
     for i, losses in enumerate(loss_hist): 
@@ -89,6 +86,6 @@ def main():
         plt.savefig("psnr_" + str(i)+".png")
         plt.clf()
         avg = np.mean(losses)
-        print("avg_psnr_" + str(i) + str(avg))
+        print("avg_psnr_" + str(i) + ": " + str(avg))
 if __name__ == "__main__": 
     main()
